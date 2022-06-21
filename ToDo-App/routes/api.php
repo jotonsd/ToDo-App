77f3d31bd5ab70877db7fd90fecc58f4f3461005
Joton Sutradhar <jotonsutradharjoy@gmail.com>
2022-06-21 20:33:34 +0600

    Update

--- ToDo-App/routes/api.php
+++ ToDo-App/routes/api.php
@@ -20,25 +20,29 @@ use App\Http\Controllers\API\TaskController;
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
-Route::post('register', [PassportAuthController::class, 'register']);
-Route::post('login', [PassportAuthController::class, 'login']);
 
-// category api's
-Route::get('/get-categories/{user_id}', [CategoryController::class, 'get']);
-Route::post('/store-categories', [CategoryController::class, 'store']);
-Route::get('/get-category/{id}', [CategoryController::class, 'get_category_by_id']);
-Route::post('/update-category/{id}', [CategoryController::class, 'update']);
 
-// task api's
-Route::get('/task/get-categories/{user_id}', [TaskController::class, 'get_category']);
-Route::get('/get-tasks/{user_id}', [TaskController::class, 'get']);
-Route::post('/store-tasks', [TaskController::class, 'store']);
-Route::get('/get-task/{id}', [TaskController::class, 'get_task_by_id']);
-Route::post('/update-task/{id}', [TaskController::class, 'update']);
-Route::get('/task-complete/{id}/{user_id}', [TaskController::class, 'complete']);
+Route::group(['middleware' => ['cors', 'json.response']], function () {
+	Route::post('register', [PassportAuthController::class, 'register']);
+	Route::post('login', [PassportAuthController::class, 'login']);
 
-//pinned tasks api's
-Route::get('/get-pinned-tasks/{user_id}', [TaskController::class, 'pinned_tasks']);
+	// category api's
+	Route::get('/get-categories/{user_id}', [CategoryController::class, 'get']);
+	Route::post('/store-categories', [CategoryController::class, 'store']);
+	Route::get('/get-category/{id}', [CategoryController::class, 'get_category_by_id']);
+	Route::post('/update-category/{id}', [CategoryController::class, 'update']);
 
-//important tasks api's
-Route::get('/get-important-tasks/{user_id}', [TaskController::class, 'important_tasks']);
+	// task api's
+	Route::get('/task/get-categories/{user_id}', [TaskController::class, 'get_category']);
+	Route::get('/get-tasks/{user_id}', [TaskController::class, 'get']);
+	Route::post('/store-tasks', [TaskController::class, 'store']);
+	Route::get('/get-task/{id}', [TaskController::class, 'get_task_by_id']);
+	Route::post('/update-task/{id}', [TaskController::class, 'update']);
+	Route::get('/task-complete/{id}/{user_id}', [TaskController::class, 'complete']);
+
+	//pinned tasks api's
+	Route::get('/get-pinned-tasks/{user_id}', [TaskController::class, 'pinned_tasks']);
+
+	//important tasks api's
+	Route::get('/get-important-tasks/{user_id}', [TaskController::class, 'important_tasks']);
+});
